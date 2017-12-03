5790ee1555ad38c68ac1c81404bf5c53297117cf
RinedJob <rinedjob@gmail.com>
2017-12-03 23:48:42 +0300

    Just do it

--- src/main/java/com/icl/yurec/dictionary/impl/PropertiesDictionary.java
+++ src/main/java/com/icl/yurec/dictionary/impl/PropertiesDictionary.java
@@ -16,7 +16,6 @@ public class PropertiesDictionary implements Dictionary {
     private final Map<String, String> dictionary = new HashMap<>();
 
     public PropertiesDictionary(String name) {
-//        InputStream input = PropertiesDictionary.class.getResourceAsStream(name);
         InputStream input = PropertiesDictionary.class.getResourceAsStream(String.format("/%s",name));
         Properties properties = new Properties();
         try {
